fa3c2aeb72c7aefa698d6f02008651db1a22e9fb
Muhammet Tarık DUMANLI <mtdumanli@gmail.com>
2024-07-12 15:41:20 +0300

    header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { appConfig, apiConfig } from '../config';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  @Input() currentUnitSystem: string = "";
+  @Input() changeUnit: EventEmitter<string> = new EventEmitter();
+
+  isUnitSwitcherChecked = false;
+
+  ngOnInit(): void {
+    this.isUnitSwitcherChecked = this.currentUnitSystem === appConfig.defaultUnit;
+  }
+
+  onChangeUnitSwitcher() {
+    const unitSystems = Object.keys(apiConfig.measurementUnits);
+    const unitIndex = this.isUnitSwitcherChecked ? 1 : 0;
+
+    this.changeUnit.emit(unitSystems[unitIndex]);
+  }
 
 }
