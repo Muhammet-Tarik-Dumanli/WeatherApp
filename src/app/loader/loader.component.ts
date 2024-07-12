8add209e73b43d9fdfd6489617d9c5a86ea285c9
Muhammet Tarık DUMANLI <mtdumanli@gmail.com>
2024-07-12 16:46:37 +0300

    loader component

--- src/app/loader/loader.component.ts
+++ src/app/loader/loader.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { LoaderService } from './loader.service';
+import { LoaderState } from './loader';
 
 @Component({
   selector: 'app-loader',
   templateUrl: './loader.component.html',
-  styleUrl: './loader.component.scss'
+  styleUrl: './loader.component.scss',
 })
-export class LoaderComponent {
+export class LoaderComponent implements OnInit, OnDestroy {
+  show = false;
+  private subscription: any; //Subscription
 
+  constructor(private loaderService: LoaderService) {}
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  ngOnInit(): void {
+    this.subscription = this.loaderService.loaderState.subscribe(
+      (state: LoaderState) => {
+        this.show = state.show;
+      }
+    );
+  }
 }
