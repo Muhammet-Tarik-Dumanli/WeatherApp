62c774ba3a6dbfa64dcdedf18136e56a2daff64c
Muhammet Tarık DUMANLI <mtdumanli@gmail.com>
2024-07-12 11:19:05 +0300

    fixed  references

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CityCardComponent } from './city-card/city-card.component';
 import { ClockComponent } from './clock/clock.component';
-import { DateComponent } from './date/date.component';
+import { DateComponent } from './date/date.component'; 
 import { ErrorComponent } from './error/error.component';
 import { ForecastComponent } from './forecast/forecast.component';
 import { HeaderComponent } from './header/header.component';
 import { LoaderComponent } from './loader/loader.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
-import { SharedComponent } from './shared/shared.component';
 import { WeatherComponent } from './weather/weather.component';
-import { ForecastItemComponent } from './forecast-item/forecast-item.component';
+import { ForecastItemComponent } from './forecast/forecast-item.component'; 
 
 @NgModule({
   declarations: [
@@ -28,7 +27,6 @@ import { ForecastItemComponent } from './forecast-item/forecast-item.component';
     LoaderComponent,
     NotFoundComponent,
     SearchBarComponent,
-    SharedComponent,
     WeatherComponent,
     ForecastItemComponent
   ],
