ffc9ce9a247728acfc392ad262f5d8ba6e7866a9
Muhammet Tarık DUMANLI <mtdumanli@gmail.com>
2024-07-12 11:30:31 +0300

    date component

--- src/app/date/date.component.ts
+++ src/app/date/date.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { DateService } from './date.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-date',
   templateUrl: './date.component.html',
-  styleUrl: './date.component.scss'
+  styleUrl: './date.component.css',
 })
-export class DateComponent {
+export class DateComponent implements OnInit, OnDestroy {
+  private _clockSubscription: any; //Subscription
+  date: string = '';
 
+  constructor(private dateService: DateService) {}
+
+  ngOnInit(): void {
+    this._clockSubscription = this.dateService
+      .getDate()
+      .subscribe(
+        (date: Date) => (this.date = this.dateService.formatDate(date))
+      );
+  }
+
+  ngOnDestroy(): void {
+    this._clockSubscription.unsubscribe();
+  }
 }
--- src/app/date/date.service.ts
+++ src/app/date/date.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { interval, Observable } from 'rxjs';
+import { map, startWith, share } from 'rxjs/operators';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class DateService {
+  private date: Observable<Date>;
+  private locale = 'en-us';
 
-  constructor() { }
+  constructor() {
+    this.date = interval(1000).pipe(
+      startWith(0),
+      map(() => new Date()),
+      share()
+    );
+  }
+
+  getDate(): Observable<Date> {
+    return this.date;
+  }
+
+  formatDate(date: Date): string {
+    const month = date.toLocaleString(this.locale, { month: 'short' });
+    const weekday = date.toLocaleString(this.locale, { weekday: 'short' });
+    const day = date.toLocaleString(this.locale, { day: 'numeric' });
+
+    return `${weekday} ${month}.${day}`;
+  }
 }
