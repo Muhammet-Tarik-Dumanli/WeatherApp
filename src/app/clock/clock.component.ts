4e5daf27273fd2fed57435912c69ebd79fce45f0
Muhammet Tarık DUMANLI <mtdumanli@gmail.com>
2024-07-12 11:29:17 +0300

    clock component

--- src/app/clock/clock.component.ts
+++ src/app/clock/clock.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ClockService } from './clock.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-clock',
   templateUrl: './clock.component.html',
-  styleUrl: './clock.component.scss'
+  styleUrl: './clock.component.css'
 })
-export class ClockComponent {
+export class ClockComponent implements OnInit, OnDestroy{
+  private _clockSubscription: any; //Subscription
+  time: any; //Date
 
+  constructor(private clockService: ClockService) {} 
+
+  ngOnInit(): void {
+    this._clockSubscription = this.clockService.getClock().subscribe(time => this.time = time);
+  }
+
+  ngOnDestroy(): void {
+    this._clockSubscription.unsubscribe();
+  }
 }
--- src/app/clock/clock.service.ts
+++ src/app/clock/clock.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { interval } from 'rxjs';
+import { map, startWith, share } from 'rxjs/operators';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class ClockService {
 
-  constructor() { }
+  private clock: Observable<Date>; 
+  constructor() { 
+    this.clock = interval(1000).pipe(
+      startWith(0),
+      map(() => new Date()),
+      share()
+    );
+  }
+
+  getClock(): Observable<Date> {
+    return this.clock;
+  }
 }
