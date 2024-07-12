810ed53fec6b7ec50601df703930519598fe5352
Muhammet Tarık DUMANLI <mtdumanli@gmail.com>
2024-07-12 11:31:37 +0300

    error component

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html',
-  styleUrl: './error.component.scss'
+  styleUrl: './error.component.css'
 })
 export class ErrorComponent {
+  title: string = '';
+  message: string = '';
 
+  constructor(route: ActivatedRoute) {
+    route.data.subscribe((data) => {
+      this.title = data["title"];
+      this.message = data["message"];
+    })
+  }
 }
