dbf7eabeeedf42bfb81d7d31e8000dec43a3ee91
Muhammet Tarık DUMANLI <mtdumanli@gmail.com>
2024-07-16 15:44:34 +0300

    resolve-location service

--- src/app/shared/services/resolve-location.service.ts
+++ src/app/shared/services/resolve-location.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Resolve, Router } from '@angular/router';
+import { Observable, EMPTY, from } from 'rxjs';
+import { catchError, switchMap } from 'rxjs';
+import { WeatherService } from '../../weather/weather.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ResolveLocationService {
+export class ResolveLocationService implements Resolve<any> {
 
-  constructor() { }
+  constructor(private weatherService: WeatherService, private router: Router) { };
+
+  resolve(): Observable<any> {
+    return from(this.weatherService.getWeatherByCurrentLocation()).pipe(
+      switchMap((city: string) => {
+        this.router.navigate([`/${city}`]);
+        return EMPTY;  // Navigation yaptıktan sonra boş bir Observable döndür
+      }),
+      catchError(error => {
+        console.error(error);
+        return EMPTY;  // Hata durumunda da boş bir Observable döndür
+      })
+    );
+  }
 }
