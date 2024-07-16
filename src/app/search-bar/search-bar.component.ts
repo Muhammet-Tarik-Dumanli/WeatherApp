920a868b14bf109b765d179a97e2d8ff9d01c765
Muhammet Tarık DUMANLI <mtdumanli@gmail.com>
2024-07-16 15:33:17 +0300

    search-bar component

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-bar',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './search-bar.component.scss'
 })
 export class SearchBarComponent {
+  searchText = '';
 
+  constructor(private router: Router) {}
+
+  onClickClearBtn() {
+    this.searchText = '';
+  }
+
+  onKeyPress(e: any) {
+    if(e.keyCode === 13 && e.target.value) {
+      const city = e.target.value;
+
+      this.router.navigate([`/$[city]`]);
+      this.searchText = '';
+    }
+  }
 }
